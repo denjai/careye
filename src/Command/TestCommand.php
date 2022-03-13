b1a424b5d05ebfcf058dff1ca7919e2d4a3c7b35
denjai <doncho.t@gmail.com>
2022-03-13 10:53:39 +0200

    configure messages, add car functionality

--- src/Command/TestCommand.php
+++ src/Command/TestCommand.php
@@ -8,17 +8,31 @@ use App\Repository\CarRepository;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Notifier\ChatterInterface;
+use Symfony\Component\Notifier\Message\ChatMessage;
+use Symfony\Component\Notifier\Notification\Notification;
+use Symfony\Component\Notifier\NotifierInterface;
+use Symfony\Component\Notifier\Recipient\Recipient;
+use Symfony\Component\Notifier\Bridge\Discord\DiscordOptions;
+use Symfony\Component\Notifier\Bridge\Discord\Embeds\DiscordEmbed;
+use Symfony\Component\Notifier\Bridge\Discord\Embeds\DiscordFieldEmbedObject;
+use Symfony\Component\Notifier\Bridge\Discord\Embeds\DiscordFooterEmbedObject;
+use Symfony\Component\Notifier\Bridge\Discord\Embeds\DiscordMediaEmbedObject;
 
 class TestCommand extends Command
 {
     private CarRepository $carRepository;
+    private NotifierInterface $notifier;
+    private ChatterInterface $chatter;
     private $start;
 
-    public function __construct(CarRepository $carRepository)
+    public function __construct(CarRepository $carRepository, NotifierInterface $notifier, ChatterInterface $chatter)
     {
         parent::__construct();
         $this->carRepository = $carRepository;
         $this->start = microtime(true);
+        $this->notifier = $notifier;
+        $this->chatter = $chatter;
     }
 
     protected function configure(): void
@@ -34,6 +48,48 @@ class TestCommand extends Command
         var_dump($car);
         var_dump(microtime(true) - $start);
         var_dump(microtime(true) - $this->start);
+
+        $notification = (new Notification('New Invoice rekt', ['chat/discord']));
+        // Send the notification to the recipient
+        $this->notifier->send($notification);
+
+        //chat message
+        $chatMessage = new ChatMessage('teta ole');
+
+        // Create Discord Embed
+        $discordOptions = (new DiscordOptions())
+            ->username('connor bot')
+            ->addEmbed((new DiscordEmbed())
+                ->color(2021216)
+                ->title('New song added!')
+                ->thumbnail((new DiscordMediaEmbedObject())
+                    ->url('https://i.scdn.co/image/ab67616d0000b2735eb27502aa5cb1b4c9db426b'))
+                ->addField((new DiscordFieldEmbedObject())
+                    ->name('Track')
+                    ->value('[Common Ground](https://open.spotify.com/track/36TYfGWUhIRlVjM8TxGUK6)')
+                    ->inline(true)
+                )
+                ->addField((new DiscordFieldEmbedObject())
+                    ->name('Artist')
+                    ->value('Alasdair Fraser')
+                    ->inline(true)
+                )
+                ->addField((new DiscordFieldEmbedObject())
+                    ->name('Album')
+                    ->value('Dawn Dance')
+                    ->inline(true)
+                )
+                ->footer((new DiscordFooterEmbedObject())
+                    ->text('Added ...')
+                    ->iconUrl('https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Spotify_logo_without_text.svg/200px-Spotify_logo_without_text.svg.png')
+                )
+            )
+        ;
+
+        // Add the custom options to the chat message and send the message
+        $chatMessage->options($discordOptions);
+
+        $this->chatter->send($chatMessage);
 
         return 0;
     }
--- src/Controller/CarController.php
+++ src/Controller/CarController.php
@@ -6,32 +6,42 @@ namespace App\Controller;
 
 use App\Repository\CarHistoryRepository;
 use App\Repository\CarRepository;
+use App\Services\CarClient;
+use App\Services\CarResultTransformer;
+use Doctrine\ORM\EntityManagerInterface;
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Profiler\Profiler;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CarController extends AbstractController
 {
     private CarRepository $carRepository;
     private CarHistoryRepository $carHistoryRepository;
+    private CarClient $carClient;
+    private CarResultTransformer $carResultTransformer;
+    private EntityManagerInterface $entityManager;
 
-    public function __construct(CarRepository $carRepository, CarHistoryRepository $carHistoryRepository)
-    {
+    public function __construct(
+        CarRepository $carRepository,
+        CarHistoryRepository $carHistoryRepository,
+        CarClient $carClient,
+        CarResultTransformer $carResultTransformer,
+        EntityManagerInterface $entityManager
+    ) {
         $this->carRepository = $carRepository;
         $this->carHistoryRepository = $carHistoryRepository;
+        $this->carClient = $carClient;
+        $this->carResultTransformer = $carResultTransformer;
+        $this->entityManager = $entityManager;
     }
 
     /**
-     * @Route("/", methods={"GET"})
+     * @Route("/", methods={"GET"}, name="list-cars")
      */
-    public function listCars(?Profiler $profiler): Response
+    public function listCars(): Response
     {
-        if (null !== $profiler) {
-            // if it exists, disable the profiler for this particular controller action
-            $profiler->disable();
-        }
-
         $cars = $this->carRepository->findAll();
         //TODO add pagination
         return $this->render('cars.html.twig', ['cars' => $cars]);
@@ -46,5 +56,37 @@ class CarController extends AbstractController
         $carHistory = $this->carHistoryRepository->findBy(['car' => $car]);
 
         return $this->render('car.html.twig', ['car' => $car, 'carHistory' => $carHistory]);
+    }
+
+    /**
+     * @Route("car", methods={"POST"}, name="add-car")
+     */
+    public function addCar(Request $request): Response
+    {
+        $id = $request->request->get('id');
+
+        if (preg_match('/\d{15,22}/', $id, $matches)) {
+            $id = $matches[0];
+        }
+
+        try {
+            $carInfo = $this->carClient->getCarInfo($id);
+            $car = $this->carRepository->findOneBy(['remoteId' => $id]);
+
+            if ($car === null) {
+                $car = $this->carResultTransformer
+                    ->transform($carInfo)
+                    ->setRemoteId($id)
+                ;
+                $this->entityManager->persist($car);
+                $this->entityManager->flush();
+            }
+        } catch (Exception $exception) {
+            return new Response($exception->getMessage());
+            //TODO show error message on page
+            // $this->session->getFlashBag()->add('error', 'user.logout');
+        }
+
+        return $this->redirectToRoute('list-cars');
     }
 }
